25a07dbdefab25b779d2dfabf6ba918bf28723b1
Pedro Tissone <pedrotissone@gmail.com>
2023-04-04 15:26:13 -0300

    cambio de img a png a ver si soluciona el problema de los navegadores

--- src/componentes/PhantomBall.js
+++ src/componentes/PhantomBall.js
@@ -13,21 +13,19 @@ function PhantomBall() {
 	const { nombreDeUsuario, puntaje } = useContext(myContext)
 
 
-	const [color, setColor] = useState("red")
+	const [className, setClassName] = useState("phantomBall_start")
 
-	function changeColor() {
-		if (color == "red") {
-			setColor("yellow")
-		} else if (color == "yellow") {
-			setColor("white")
+	function changeClassName() {
+		if (className == "phantomBall_start") {
+			setClassName("phantomBall_invert")		
 		} else {
-			setColor("red")
+			setClassName("phantomBall_start")
 		}
 	}
 
 	function handleClick(e) {
 		if (count > 0 && mostrarBoton == false) {			
-			changeColor()
+			changeClassName()
 			sumar()
 		} else {
 			
@@ -48,46 +46,46 @@ function PhantomBall() {
 	const [count, setCount] = useState(20)
 											
 
-	useEffect(() => {
-		if (count > 0) {
-		const timer = setTimeout(() => {
-			if (timeLeft > 0) {
-				setTimeLeft(timeLeft - 1)
-			} else {				
-				Swal.fire({
-					titleText: "El juego terminó, ya no tiene más clicks",
-					icon: "warning",
-					iconColor: "red",
-					background: "#17202A",
-					color: "#fff",					
-					confirmButtonColor:"#17202A",
-					})
-				setMostrarBoton(true)
-			}	
-		}, 1000)
-	} else {
-		setTimeLeft(0)
-		setMostrarBoton(true)
-	}}, [timeLeft])
+	// useEffect(() => {
+	// 	if (count > 0) {
+	// 	const timer = setTimeout(() => {
+	// 		if (timeLeft > 0) {
+	// 			setTimeLeft(timeLeft - 1)
+	// 		} else {				
+	// 			Swal.fire({
+	// 				titleText: "El juego terminó, ya no tiene más clicks",
+	// 				icon: "warning",
+	// 				iconColor: "red",
+	// 				background: "#17202A",
+	// 				color: "#fff",					
+	// 				confirmButtonColor:"#17202A",
+	// 				})
+	// 			setMostrarBoton(true)
+	// 		}	
+	// 	}, 1000)
+	// } else {
+	// 	setTimeLeft(0)
+	// 	setMostrarBoton(true)
+	// }}, [timeLeft])
 
 
-	useEffect(() => {		
-		  const intervalo = setInterval(() => {					
+	// useEffect(() => {		
+	// 	  const intervalo = setInterval(() => {					
 
-			setPosicionTotal({
-			top: Math.floor(Math.random() * (window.innerHeight - 45)),
-			right: Math.floor(Math.random() * (window.innerWidth - 45)),
-			bottom: Math.floor(Math.random() * (window.innerHeight - 45)),
-			left: Math.floor(Math.random() * (window.innerWidth - 45)),
-			});
-		}, 1000); 	
+	// 		setPosicionTotal({
+	// 		top: Math.floor(Math.random() * (window.innerHeight - 45)),
+	// 		right: Math.floor(Math.random() * (window.innerWidth - 45)),
+	// 		bottom: Math.floor(Math.random() * (window.innerHeight - 45)),
+	// 		left: Math.floor(Math.random() * (window.innerWidth - 45)),
+	// 		});
+	// 	}, 1000); 	
 
-	setTimeout(() => {
-		clearInterval(intervalo);			
-		setMostrarBoton(true)		
-	}, 20000);
+	// setTimeout(() => {
+	// 	clearInterval(intervalo);			
+	// 	setMostrarBoton(true)		
+	// }, 20000);
 
-	return () => clearInterval(intervalo);}, []);	
+	// return () => clearInterval(intervalo);}, []);	
 	
 
 
@@ -114,16 +112,19 @@ function PhantomBall() {
 
 
   return(
-      <div onClick={handlecount} className="phantomBall_contenedor">
+      <div  onClick={handlecount} className="phantomBall_contenedor">
 				<div className="phantomball_divInfo">
 					<p className="phantomball_count">CLICKS: {count}</p>
 					<p className="phantomBall_time"> TIEMPO: {timeLeft}</p>
 					<p className="phantomball_puntaje">PUNTOS: {puntaje}</p>
 				</div>
 				
-				<svg className="phantomBall_start" onClick={handleClick} style={{  color: color, position: 'absolute', top: `${posicionTotal.top}px`, left: `${posicionTotal.left}px`, bottom: `${posicionTotal.bottom}px`, right: `${posicionTotal.rigth}px` }} width="17" height="17" viewBox="0 0 17 17"  xmlns="http://www.w3.org/2000/svg">
-				{ mostrarBoton == false && <circle cx="8.5" cy="8.5" r="8.5" fill={color} /> }
-				</svg> 
+				{/* // <svg className="phantomBall_start" onClick={handleClick} style={{  color: color, position: 'absolute', top: `${posicionTotal.top}px`, left: `${posicionTotal.left}px`, bottom: `${posicionTotal.bottom}px`, right: `${posicionTotal.rigth}px` }} width="17" height="17" viewBox="0 0 17 17"  xmlns="http://www.w3.org/2000/svg">
+				// { mostrarBoton == false && <circle cx="8.5" cy="8.5" r="8.5" fill={color} /> }
+				// </svg> */}
+
+				{ mostrarBoton == false && <img src="./circulo.png" className={className} onClick={handleClick} style={{ position: 'absolute', top: `${posicionTotal.top}px`, left: `${posicionTotal.left}px`, bottom: `${posicionTotal.bottom}px`, right: `${posicionTotal.right}px` }}/>}
+				
 				
 				<div className="phantomball_divMostrarBoton">
 					<Link to={"/resultado"}>{mostrarBoton && <button className="phantomBall_mostrarBoton">Ver resultado</button>}</Link>
